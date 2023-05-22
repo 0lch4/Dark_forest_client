8625504817904728fd6b4a99c6e4b3fb16a41918
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-05-22 16:06:10 +0200

    client optymalization

--- main.py
+++ main.py
@@ -2,87 +2,72 @@ import requests
 from getpass import getpass
 from bs4 import BeautifulSoup
 
-def create_user(username, password):
-    session = requests.session()
 
-    create_user_url = 'http://127.0.0.1:8000/stats/create_user'
-    response = session.get(create_user_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    csrf_token = soup.find('input', attrs={'name': 'csrfmiddlewaretoken'})['value']
-
-    data = {
-        'csrfmiddlewaretoken': csrf_token,
-        'username': username,
-        'password': password,
-    }
-
-    response = session.post(create_user_url, data=data)
-
-    if response.status_code == 200:
-        print('Account created successfully')
-    else:
-        print(f'Error {response.status_code} pleas contact to administrator https://github.com/0lch4')
+class Connection:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
         
+    def conn(self,link,text):
+        self.session = requests.session()
+        self.response=self.session.get(link)
+        soup = BeautifulSoup(self.response.content, 'html.parser')
+        self.csrf_token = soup.find('input', attrs={'name': 'csrfmiddlewaretoken'})['value']
+        self.data = {
+        'csrfmiddlewaretoken': self.csrf_token,
+        'username': self.username,
+        'password': self.password,
+        }
+        self.response = self.session.post(link, data=self.data)
+        if self.response.status_code == 200:
+            print(text)
+        else:
+            print(f'Error {self.response.status_code} pleas contact to administrator https://github.com/0lch4')
         
-def login_user(username, password):
-    session = requests.session()
-
-    login_url = 'http://127.0.0.1:8000/stats/login'
-    response = session.get(login_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    csrf_token = soup.find('input', attrs={'name': 'csrfmiddlewaretoken'})['value']
-
-    data = {
-        'csrfmiddlewaretoken': csrf_token,
-        'username': username,
-        'password': password,
-    }
-
-    response = session.post(login_url, data=data)
-
-    if response.status_code == 200:
-        print('you successfully logged in')
-    else:
-        print(f'Error {response.status_code} pleas contact to administrator https://github.com/0lch4')
-        
-def update(username, new_score):
-    update_stats_url = 'http://127.0.0.1:8000/stats/modify'
-    data = {
-        'username': username,
+    def update(self,new_score,link,text):
+        self.data = {
+        'username': self.username,
         'best_score': new_score,
-    }
+        }
+        self.response = requests.post(link, data=self.data)
+        if self.response.status_code == 200:
+            print(text)
+        else:
+            print(f'Error {self.response.status_code} pleas contact to administrator https://github.com/0lch4')
+        
+    def show(self,link):
+        self.response = requests.get(link)
+        if self.response.status_code == 200:
+            self.stats = self.response.json()
+            print(self.stats)
+        else:
+            print(f'Error {self.response.status_code} pleas contact to administrator https://github.com/0lch4')
 
-    response = requests.post(update_stats_url, data=data)
-    if response.status_code == 200:
-        print('Stats have been updated')
-    else:
-        print(f'Error {response.status_code} pleas contact to administrator https://github.com/0lch4')
+texts=[
+    'Account created successfully',
+    'You succesfully logged in',
+    'Stats has been updated',
+]
 
-def show():
-    show_stats_url = 'http://127.0.0.1:8000/stats/show'
-    response = requests.get(show_stats_url)
-    if response.status_code == 200:
-        serialized_stats = response.json()
-        print(serialized_stats)
-    else:
-        print(f'Error {response.status_code} pleas contact to administrator https://github.com/0lch4')
-
-
-def register():
-    username = input('Type your username: ')
-    password = getpass('Type your password: ')
-    create_user(username, password)
-    
-def login():
-    username = input('Type your username: ')
-    password = getpass('Type your password: ')
-    login_user(username, password)
+links=[
+    'http://127.0.0.1:8000/stats/create_user',
+    'http://127.0.0.1:8000/stats/login',
+    'http://127.0.0.1:8000/stats/modify',
+    'http://127.0.0.1:8000/stats/show',
+]
 
 def main():
-    #register()
-    #login()
-    update('janek','5')
-    show()
+    username = input('Type your username: ')
+    password = getpass('Type your password: ')
+    acc = Connection(username,password)
+    #create user
+    acc.conn(links[0],texts[0])
+    #login user
+    acc.conn(links[1],texts[1])
+    #update user's stats
+    acc.update(12,links[2],texts[2])
+    #show global stats
+    acc.show(links[3])
     
 
 if __name__ == '__main__':
